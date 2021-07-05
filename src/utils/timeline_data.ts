9d0833480a1f4a0a401b1640cb47cd52da5d7fb6
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2021-07-05 15:45:50 -0300

    act: ajuste de textos e datas capstone Q2A

--- src/utils/timeline_data.ts
+++ src/utils/timeline_data.ts
@@ -50,33 +50,33 @@ const data: Data[] = [
     title: "5° dia do projeto",
     type: "Dia de código e gerenciamento!",
     date: "08/07 - Quinta feira",
-    content: "O quinto dia de projeto será além de continuar as tarefas para construir a aplicação, também será necessário que o grupo realize uma retrospectiva e tenha um tempo para refletir e entender o que poderia ter sido melhor, e como o projeto progrediu como um todo.",
+    content: "O quinto dia de projeto será além de continuar as tarefas para construir a aplicação, também será necessário que o grupo realize uma retrospectiva e tenha um tempo para refletir e entender o que poderia ter sido melhor, e como o projeto progrediu como um todo. Este documento deve ser anexado ao Kanban (Trello)",
     week: "Primeira semana",
-    hasAssignment: true,
-    itemAssignment: 'https://alunos.kenzie.com.br/courses/28/assignments/3256?module_item_id=3658'
+    hasAssignment: false,
   },
   {
     title: "6° dia do projeto",
     type: "Dia de código",
     date: "09/07 - sexta feira",
     content:
-      "O dia 04 será o primeiro dia em que o grupo irá começar a codar. Após separar as tasks, mapear requisitos, entender o problema, e todos estarem entendendo a solução, é dada a largada para abrir o seu editor e botar a mão na massa.",
+      "O sexto dia deverá ter um planejamento do que será feito na semana, e partir para o código, visando construir o cerne da aplicação de proposta.",
     week: "Primeira semana",
     hasAssignment: false,
   },
   {
     title: "7° dia do projeto",
-    type: "Dia de código",
-    date: "11/07 - Segunda feira",
+    type: "Dia de código e pre-apresentação",
+    date: "12/07 - Segunda feira",
     content:
-      "O sétimo dia será um dia além de continuar as features, terá um momento de apresentação do projeto para um cliente (Equipe de ensino). O que será verificado: A situação de como está indo em relação ao planejamento, como o código está estruturado, e a organização geral das ferramentas (Kanban, Figma, etc). No botão abaixo está o calendário de reuniões com os clientes.",
+      "O sétimo dia terá um momento de apresentação do projeto para um cliente (Equipe de ensino). O que será verificado: A situação de como está indo em relação ao planejamento, como o código está estruturado, e a organização geral das ferramentas (Kanban, Figma, etc). No botão abaixo está o calendário de reuniões com os clientes.",
     week: "Segunda semana",
     hasAssignment: true,
+    itemAssignment: 'https://www.notion.so/kenzieacademybr/Calend-rio-de-pr-apresenta-es-e262683cf2084a029195ea4671feeee5'
   },
   {
     title: "8° dia do projeto",
     type: "Dia de código",
-    date: "12/07 - Terça feira",
+    date: "13/07 - Terça feira",
     content:
       "O oitavo dia é mais um dia focado no código, lembrando sempre de seguir o planejamento, reportar dificuldades e focar no MVP.",
     week: "Segunda semana",
@@ -85,20 +85,21 @@ const data: Data[] = [
   {
     title: "9° dia do projeto",
     type: "Dia de código",
-    date: "03/07 - Quarta feira",
+    date: "14/07 - Quarta feira",
     content:
-      "O nono dia é o dia final para a entrega do código, onde o grupo deverá entregar o repositório contendo a solução proposta.",
+      "O nono dia é para corrigir possíveis bugs na aplicação, finalizar o layout e deixar o código limpo sem mensagens no console e comentários desnecessários.",
     week: "Segunda semana",
-    hasAssignment: true,
+    hasAssignment: false,
   },
   {
     title: "10° dia do projeto",
     type: "Dia de código",
-    date: "07/07 - Quinta feira",
+    date: "15/07 - Quinta feira",
     content:
       "O décimo dia, será o dia final do grupo trabalhando no Capstone, o grupo deverá realizar a ultima retrospectiva e se preparar para partir para um próximo desafio (Q3)! 🚀",
     week: "Segunda semana",
     hasAssignment: true,
+    itemAssignment: 'https://alunos.kenzie.com.br/courses/35/assignments/7326?module_item_id=8397'
   },
 ];
 
